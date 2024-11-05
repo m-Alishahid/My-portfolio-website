4a604041d537e0ea651b7663b5c73f1b21df8181
Muhammad Ali Shahid <160266513+m-Alishahid@users.noreply.github.com>
2024-11-05 10:26:15 -0800

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -138,7 +138,7 @@ const listItemsVariants={
              variants={listVariants}
               initial="closed" 
               animate="opened"
-               className='absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl z-40'>
+               className='absolute top-0 left-0 w-screen min-h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl z-40'>
            
            
             {links.map((link)=>( 
