e3018162c4d865824879d7d546e66145ea25b0fb
codecraftali <160266513+m-Alishahid@users.noreply.github.com>
2025-04-03 09:43:51 -0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,7 @@ import TransitionProvider from "@/components/transitionprovider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Meet Ali shahid",
+  title: "Meet Ali shahid a front-end developer and ui ux designer from pakistan",
   description: "Animated portfolio",
 };
 
